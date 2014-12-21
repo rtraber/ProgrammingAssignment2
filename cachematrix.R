baef802f09736b37926b4c8c8f37c53365d6c4f2
rtraber <rolf.traber@hotmail.com>
2014-12-21 13:52:16 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## function makeCacheMatrix creates cache object of matrix and its iverse.
+## the cache object name is the hash of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        require(digest)
+        digfn<-sprintf("m_%s.digest",digest(x))         # create unique hash used as name
+        invers<-NA                                      # init value if not invertible
+        try(invers<-solve(x),silent=TRUE)               # invert, suppress any error
+        assign(digfn,list(m=x,i=invers), envir = .GlobalEnv)    # save/cache result as list
+        invers     
 }
 
-
-## Write a short comment describing this function
-
+## function cacheSolve: returns inverse of matrix x
+## before inverting x lookup cache using searching hash object of x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        require(digest)
+        digfn<-sprintf("m_%s.digest",digest(x))         # create unique hash used as var name
+        if (exists(digfn)) {
+                invers<-get(digfn)$i                    # get result / was already inverted
+        } else {
+                invers<-makeCacheMatrix(x)              # calculate result and cache
+        }
+        invers                                          # return value m inverted
 }
